2a00a5e116e093f504a8f324a69ed611ed8266d7
kappashun <kappashun@hotmail.com>
2021-11-27 12:57:27 +0300

    changes, optimizing

--- F1_lap_times.py
+++ F1_lap_times.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
 from datetime import datetime, timedelta
-import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
 pd.options.mode.chained_assignment = None
-from scipy.interpolate import make_interp_spline, BSpline
 
 """
 Module to work with time in F1 races
@@ -20,77 +20,18 @@ In F1 distance between drivers measures by time
 lap_times = pd.read_csv('lap_times.csv')
 
 # Dropping useless rows
-lap_times = lap_times.drop(['milliseconds', 'driverId'], axis=1)[lap_times['raceId'] < 2]
-
-#Table with time intervals p2 over p1 on every lap in every race
-lap_intervals = lap_times[['raceId', 'lap']].drop_duplicates()
-length = list(lap_intervals.index.values)
+lap_times = lap_times.drop(['milliseconds', 'driverId'], axis=1)
 
 
-def interval(i):
-    """
-    Function helps to build 'interval' column in lap_intervals table
-
-    :param i: Index of unique pair of racedId and number of lap
-
-    :return: Time gap change (in readable string format)
-             of lap time from p2 to p1
-    """
-
-    time1 = lap_times[(lap_times['raceId'] == lap_intervals.loc[i]['raceId']) &
-              (lap_times['lap'] == lap_intervals.loc[i]['lap']) &
-              (lap_times['position'] == 1)]['time']
-    time2 = lap_times[(lap_times['raceId'] == lap_intervals.loc[i]['raceId']) &
-              (lap_times['lap'] == lap_intervals.loc[i]['lap']) &
-              (lap_times['position'] == 2)]['time']
-
-    # Time format
-    FMT = '%M:%S.%f'
-
-    time1 = time1.tolist()[0] # p1 lap time
-    time2 = time2.tolist()[0] # p2 lap time
-
-    try:
-        delta = datetime.strptime(max(time1, time2), FMT) - \
-                datetime.strptime(min(time1, time2), FMT)
-
-    # Occures if time1 or time2 not in FMT pattern,
-    # means lap time is over an hour, exceptional case
-    except ValueError:
-        delta = datetime.strptime(max(time1, time2), '%H:%M:%S.%f') - \
-                datetime.strptime(min(time1, time2), '%H:%M:%S.%f')
-
-    interval = str(delta)[3:-3]
-    if time1 < time2:
-        interval = '+' + interval
-    elif time1 > time2:
-        interval = '-' + interval
-    return interval
-
-
-def gap(i):
-    """
-    Function helps to build 'gap' column in lap_intervals table
-
-    :param i: Index of unique pair of racedId and number of lap
-
-    :return: Time difference (in readable string format)
-              of total time between p1 and p2
-    """
-    res = 0
-    for lap in range(1, lap_intervals.loc[i]['lap'] + 1):
-        res += delta_to_seconds(lap_intervals[(lap_intervals['lap'] == lap) &
-                                              (lap_intervals['raceId'] == lap_intervals.loc[i]['raceId'])
-                                              ]['interval'].tolist()[0])
-    return seconds_to_delta(res)
-
-
-def delta_to_seconds(delta: str) -> float:
+def delta_to_ms(delta: str):
     """
     :param delta: String-type time
-    :return: Float-type time
+    :return: Int-type time
 
     """
+    if delta == '--:--':
+        return 0
+
     if delta[0] == '+' or delta[0] == '-':
         parsed_time = delta[1:]
     else:
@@ -100,56 +41,88 @@ def delta_to_seconds(delta: str) -> float:
     # If delta time is over an hour, exceptional case
     if len(parsed_time) == 3:
         try:
-            result = float(parsed_time[0])*3600 + \
-                     float(parsed_time[1])*60 + \
+            result = float(parsed_time[0]) * 3600 + \
+                     float(parsed_time[1]) * 60 + \
                      float(parsed_time[2])
         except ValueError:
-            result = float(parsed_time[0].split(' ')[-1])*3600 + \
-                     float(parsed_time[1])*60 + \
+            result = float(parsed_time[0].split(' ')[-1]) * 3600 + \
+                     float(parsed_time[1]) * 60 + \
                      float(parsed_time[2])
-
+    elif len(parsed_time) == 1:
+        result = float(parsed_time[0])
     else:
-        result = float(parsed_time[0])*60 + float(parsed_time[1])
+        result = float(parsed_time[0]) * 60 + float(parsed_time[1])
 
     if delta[0] == '-':
         result = -result
-    return result
+
+    return int(result*1000)
 
 
-def seconds_to_delta(val: float) -> str:
+def ms_to_delta(val) -> str:
     """
-    :param delta: Float-type time
+    :param delta: Int-type time
     :return: String-type time
 
     """
+    if val == None:
+        return '--:--'
+
+    val = float(val)/1000
     # If delta time is over an hour, exceptional case
-    if int(abs(val)/3600) != 0:
-        hours = str(int(abs(val)/3600))
-        minutes = str(int((abs(val)%3600)/60))
-        seconds = "{:.3f}".format(abs(val)%60)
+    if int(abs(val) / 3600) != 0:
+        hours = str(int(abs(val) / 3600))
+        minutes = str(int((abs(val) % 3600) / 60))
+        seconds = "{:.3f}".format(abs(val) % 60)
         res = ':'.join([hours, minutes, seconds])
 
     else:
-        minutes = str(int(abs(val)/60))
-        seconds = "{:.3f}".format(abs(val)%60)
+        minutes = str(int(abs(val) / 60))
+        seconds = "{:.3f}".format(abs(val) % 60)
         res = ':'.join([minutes, seconds])
     return res
 
 
-# Adding columns to 'lap_intervals'
-#
-# Interval: read function 'interval' docstring
-# Gap: read function 'gap' docstring
-# Ms: gap in milliseconds
-#
-lap_intervals['interval'] = [interval(i) for i in length]
-lap_intervals['gap'] = [gap(i) for i in length]
-lap_intervals['ms'] = [int((delta_to_seconds(gap(i)))*1000) for i in length]
+def interval(id):
+    """
+    Function helps to build 'interval' column in table
 
-# Table with average gap between p1 and p2 by every lap
-average_gap = pd.DataFrame({'lap' : lap_intervals['lap'].unique(),
-                             'ms' : [int(lap_intervals[lap_intervals['lap'] == i+1]['ms'].mean())
-                                     for i in range(lap_intervals['lap'].unique().size)]})
+    :param i: Index of unique row in table
+
+    :return: Time gap change (in readable string format)
+             of lap time from driver to driver ahead
+    """
+    pos = lap_times.loc[id]['position']
+    if pos == 1:
+        return '--:--'
+
+    race_id = lap_times.loc[id]['raceId']
+    lap = lap_times.loc[id]['lap']
+
+    time1 = lap_times[(lap_times['raceId'] == race_id) &
+                      (lap_times['lap'] == lap) &
+                      (lap_times['position'] == pos - 1)]['time']
+    time2 = lap_times[(lap_times['raceId'] == race_id) &
+                      (lap_times['lap'] == lap) &
+                      (lap_times['position'] == pos)]['time']
+
+    time1 = delta_to_ms(time1.tolist()[0])  # p1 lap time
+    time2 = delta_to_ms(time2.tolist()[0])  # p2 lap time
+
+    interval = ms_to_delta(abs(time1 - time2))
+
+    if time1 < time2:
+        interval = '+' + interval
+    elif time1 > time2:
+        interval = '-' + interval
+    return interval
+
+
+# Intervals
+lap_times['interval'] = [interval(i) for i in length]
+
+# Intervals in milliseconds
+lap_times['ms'] = lap_times['interval'].map(delta_to_ms)
 
 
 def Graph():
@@ -158,17 +131,28 @@ def Graph():
     between p1 and p2 by every lap
 
     """
-    laps = list(average_gap['lap'])
-    ms = list(average_gap['ms'])
-    sec = [i/1000 for i in ms]
 
-    xnew = np.linspace(1, max(laps), 1000)
-    spl = make_interp_spline(laps, sec, k=3)
-    power_smooth = spl(xnew)
+    # Average interval per every lap, not counting exceptional cases
+    aver_int = lap_times[lap_times['position'] == 2][['lap', 'ms']]
+    aver_int = aver_int.drop(aver_int[abs(aver_int['ms']) > 200000].index)
+    aver_int = aver_int.groupby(['lap']).mean()
 
+    laps = aver_int.index
+    gaps = []
+    res = 0
+    for lap, ms in aver_int.iterrows():
+        res += ms
+        gaps.append(res.tolist()[0])
+
+    sec = [i / 1000 for i in gaps]
+    del aver_int
+
+    plt.figure(figsize=(10, 6))
     plt.title('Gap between 1 and 2 pos')
     plt.xlabel("Lap")
     plt.ylabel("Seconds")
     plt.grid(True)
-    plt.plot(xnew, power_smooth, color='red')
+    plt.axvline(60, color='blue', linestyle='--')
+    plt.text(60, 3, 'Average number of laps', rotation=-90, verticalalignment='bottom', fontsize=10)
+    plt.plot(laps, sec, color='red')
     plt.show()
